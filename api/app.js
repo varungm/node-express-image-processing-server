c70b3db03dcda1ff829308ba3da813774e10387b
varungm <43920513+varungm@users.noreply.github.com>
2023-01-06 11:23:55 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path= require('path');
 const app =express();
+const router = require('./src/router');
 
 const pathToIndex = path.resolve(__dirname,'../client/index.html');
+
+app.use('/',router);
+
+app.use(express.static(path.resolve(__dirname,'uploads')))
 
 app.use('/*',(request,response)=>{
     response.sendFile(pathToIndex);
